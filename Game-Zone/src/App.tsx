0824ae07f9028aced808ac56a2f3c9e1d5284500
Your Name <taghavinima807@gmail.com>
2026-09-02 22:54:29 +0330

    Filter Games By Platforms

--- Game-Zone/src/App.tsx
+++ Game-Zone/src/App.tsx
@@ -5,12 +5,14 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import type { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import type { Platform } from "./hooks/useGames";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import NavBar from "./NavBar";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null > (null);
   return (
     <Grid
       templateAreas={{
@@ -36,8 +38,8 @@ function App() {
       </Show>
 
       <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform) => setSelectedPlatform(platform)}  />
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
   );
