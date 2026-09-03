bb034fe960ab85104cd8de8baceea173f3708534
Your Name <taghavinima807@gmail.com>
2026-09-04 00:12:11 +0330

    Clean Up The Genres

--- Game-Zone/src/components/GenreList.tsx
+++ Game-Zone/src/components/GenreList.tsx
@@ -1,5 +1,5 @@
 import useGenres from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Button } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Button, Heading } from "@chakra-ui/react";
 import { SkeletonText, SkeletonCircle } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 import type { Genre } from "../hooks/useGenres";
@@ -18,39 +18,44 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   if (error) return null;
 
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3} >Genres</Heading>
     <List>
       {isLoading
         ? skeletons.map((skeleton) => (
-            <ListItem key={skeleton} paddingY="5px">
+          <ListItem key={skeleton} paddingY="5px">
               <HStack>
                 <SkeletonCircle boxSize="32px" />
                 <SkeletonText noOfLines={1} width="100px" />
               </HStack>
             </ListItem>
           ))
-        : data.map((genre) => (
+          : data.map((genre) => (
             <ListItem key={genre.id} paddingY="5px">
               <HStack>
                 <Image
                   boxSize="32px"
                   borderRadius={8}
+                  objectFit='cover'
                   src={getCroppedImageUrl(genre.image_background)}
-                />
-                <Button
+                  />
+                <Button whiteSpace='normal' textAlign='left' 
                   fontWeight={
                     genre.id === selectedGenre?.id ? "bold" : "normal"
                   }
                   onClick={() => onSelectGenre(genre)}
                   fontSize="lg"
                   variant="ghost"
-                >
+                  >
                   {genre.name}
                 </Button>
               </HStack>
             </ListItem>
           ))}
     </List>
+    </>
   );
 };
+
 
 export default GenreList;
